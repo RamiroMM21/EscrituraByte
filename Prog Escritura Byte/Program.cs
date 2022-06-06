461f6dcb7c7cbcb88c9c496288389ccf4d5d37f6
RamiroMM21 <105456306+RamiroMM21@users.noreply.github.com>
2022-06-06 11:01:10 -0700

    Escritura byte

---
+++ Prog Escritura Byte/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Prog_Escritura_Byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            byte[] buffer = new byte[81];
+            int nbytes = 0, car;
+
+            try
+            {
+                //crear el flujo del archivo
+                fs = new FileStream("text.txt", FileMode.Create, FileAccess.Write);
+                Console.WriteLine("Escriba el texto que desea almacenar en el archivo: ");
+                while ((car = Console.Read()) != '\r' && (nbytes < buffer.Length))
+                {
+                    buffer[nbytes] = (byte)car;
+                    nbytes++;
+
+                }
+                fs.Write(buffer, 0, nbytes);
+            }
+            catch (IOException e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
